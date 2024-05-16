40f32a1a01903455b86c01e5cb90eda390bc8234
talSagy <tal.sagy.1@gmail.com>
2024-05-16 10:07:18 +0300

    Update main.js
    Added some text and a comment

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World it's Tal')
+    /*bla bla*/
 })
 
 app.listen(3000)
